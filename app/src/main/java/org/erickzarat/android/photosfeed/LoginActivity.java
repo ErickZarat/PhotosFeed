70af8b99754f12fdc4a97d2f46331b2599e33a94
ErickZarat <ezarat15@gmail.com>
2016-07-02 15:31:09 -0600

    initial commit

---
+++ app/src/main/java/org/erickzarat/android/photosfeed/LoginActivity.java
@@ -0,0 +1,13 @@
+package org.erickzarat.android.photosfeed;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
